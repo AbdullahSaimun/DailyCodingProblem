e65ef47a23065cbde21e32fcc202721d25baef62
Abdulla Al Saimun <44600426+AbdullahSaimun@users.noreply.github.com>
2024-03-30 01:52:28 +0600

    square sorted

---
+++ src/main/java/org/problem/solving/all/SquareSorted.java
@@ -0,0 +1,23 @@
+package org.problem.solving.all;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SquareSorted {
+
+	public static List<Integer> squareAndSort(List<Integer> nums) {
+		nums = nums.stream().map(obj -> obj * obj)
+				.collect(Collectors.toList());
+		Collections.sort(nums);
+		return nums;
+	}
+
+	public static void main(String[] args) {
+		List<Integer> nums = Arrays.asList(-9, -2, 0, 2, 3);
+		List<Integer> squaredSorted = squareAndSort(nums);
+		System.out.println(squaredSorted);
+//		[0, 4, 4, 9, 81]
+	}
+}
